b83f4e96115cd436f00b90023724d6b7d5615a88
Marsol4 <enzogmarsola@gmail.com>
2025-03-05 19:55:20 -0300

    Adiciona os arquivos já criado no spring initializer

---
+++ src/main/java/br/com/fiap/empreenda_api/EmpreendaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.empreenda_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpreendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpreendaApiApplication.class, args);
+	}
+
+}
