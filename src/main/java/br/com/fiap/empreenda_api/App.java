03bfd6ba98e0e0bcfa28b7dfda67bf6f93ad102b
MarsoL4 <enzogmarsola@gmail.com>
2025-04-10 10:27:14 -0300

    docs: OpenApiDefinition

--- src/main/java/br/com/fiap/empreenda_api/App.java
+++ src/main/java/br/com/fiap/empreenda_api/App.java
@@ -2,8 +2,16 @@ package br.com.fiap.empreenda_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(title = "EmpreendaPlus API", version = "v1", description = "API do SaaS EmpreendaPlus")
+)
+@EnableCaching
 public class App {
 
 	public static void main(String[] args) {
